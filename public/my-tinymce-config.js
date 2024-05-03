378550ebdd0c6403ba1a2756a97bc1c6b3aa26fa
threegoeseasy <threegoeseasy@gmail.com>
2024-05-03 17:11:03 +0530

    full path urls

--- public/my-tinymce-config.js
+++ public/my-tinymce-config.js
@@ -3,4 +3,7 @@ tinymce.init({
   plugins: "lists link image table code help wordcount",
   images_upload_url: "/saveImage",
   license_key: "gpl",
+  relative_urls: false,
+  remove_script_host: false,
+  convert_urls: true,
 });
